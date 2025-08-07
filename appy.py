ee1a2630bfded33eeb3fceff7bb968cd9fdb3ff8
Stacey-M <sincerely.staymi>
2025-08-06 22:02:24 -0700

    commit!

--- appy.py
+++ appy.py
@@ -3,6 +3,25 @@ app = Flask(__name__)
 
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/start")
+def start():
+    return render_template("start.html")
+
+@app.route("/G1")
+def G1():
+    return render_template("G1.html", Img1 = "Ai1.avif", Img2 = "ai2.jpg")
+
+@app.route("/win1")
+def win1():
+    return render_template("win1.html")
+
+
+
+
 # main driver function
 if __name__ == '__main__':
     app.run(debug=True)
