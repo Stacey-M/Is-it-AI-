f96563ade3eb91f78ae2d26354168326fd7071e3
Stacey-M <sincerely.staymi>
2025-08-07 13:12:18 -0700

    pewpewpew

--- appy.py
+++ appy.py
@@ -19,8 +19,33 @@ def G1():
 def win1():
     return render_template("win1.html")
 
+@app.route("/lost1")
+def lost1():
+    return render_template("lost1.html")
 
+@app.route("/G2")
+def G2():
+    return render_template("G2.html")
 
+@app.route("/win2")
+def win2():
+    return render_template("win2.html")
+
+@app.route("/lost2")
+def lost2():
+    return render_template("lost2.html")
+
+@app.route("/G3")
+def G3():
+    return render_template("G3.html")
+
+@app.route("/win3")
+def win3():
+    return render_template("win3.html")
+
+@app.route("/lost3")
+def lost3():
+    return render_template("lost3.html")
 
 # main driver function
 if __name__ == '__main__':
